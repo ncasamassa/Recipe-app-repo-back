367d3aad6cd9b75140f1951bbc59ef5317012072
jphillips1833 <justin.phillips1833@gmail.com>
2024-01-11 20:36:23 -0600

    Co-authored-by: Mavericks Balitaan <mavericks-db@users.noreply.github.com> Co-authored-by: ncasamassa <ncasamassa@users.noreply.github.com>

--- index.js
+++ index.js
@@ -1,62 +1,61 @@
-const express = require('express');
+const express = require("express");
 const server = express();
 const PORT = 3001;
 const mysql = require("mysql2");
-server.use(express.json());
 
-server.get('/', (req,res) => {
-    res.send('This is the Express Backend');
-});
+server.use(express.json());
+server.use(cors());
 
 const db = mysql.createConnection({
-    user: "root",
-    password: "",
-    host: "localhost",
-    database: "ingredientsDB",
-  });
+  user: "root",
+  password: "",
+  host: "localhost",
+  database: "ingredientsDB",
+  // table: "inventory"
+});
 
-server.get("placeholder", (req, res) => {
-    db.query("SELECT * FROM placeholder", (err, results) => {
-        if (err) {
-          console.log(err);
-        } else {
-          res.send(results);
-        }
-      });
-})
+server.get("/", (req, res) => {
+  res.send("This is the Express Backend");
+});
+
+server.get("/inventory", (req, res) => {
+  db.query("SELECT * FROM inventory", (err, results) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send(results);
+    }
+  });
+});
 
 server.post("/create", (req, res) => {
-    const first = req.body.name;
-    const second = req.body.name;
-    const third = req.body.name;
-    const fourth = req.body.name;
+  const { quantity, category, ingredient } = req.body;
 
-    db.query(
-        "INSERT INTO placeholder (first, second, third, fourth) VALUES (?,?,?,?)",
-        [first, second, third, fourth],
-        (err, result) => {
-          if (err) {
-            console.log(err);
-          } else {
-            res.send(result);
-          }
-        }
-      );
-})
-
-server.delete("/delete/:id", (req, res) => {
-    const id = req.params.id;
-  
-    db.query("DELETE FROM placeholder WHERE id = ?", id, (err, result) => {
+  db.query(
+    "INSERT INTO inventory (quantity, category, ingredient) VALUES (?,?,?)",
+    [quantity, category, ingredient],
+    (err, result) => {
       if (err) {
-        console.log(err)
+        console.log(err);
       } else {
         res.send(result);
       }
-    })
-  })
+    }
+  );
+});
 
+server.delete("/delete/:id", (req, res) => {
+  const id = req.params.id;
+
+  db.query("DELETE FROM inventory WHERE id = ?", id, (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send(result);
+    }
+  });
+});
 
 server.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
