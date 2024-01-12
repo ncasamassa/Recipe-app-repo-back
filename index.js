67d7b4f53d5c38315f4db50b34ab78faa8a6cd3e
Nick Casamassa <nickabod.crane@gmail.com>
2024-01-11 17:37:08 -0800

    Added mysql connection, create, delete capability.

--- index.js
+++ index.js
@@ -1,11 +1,62 @@
 const express = require('express');
-const app = express();
-const PORT = process.env.PORT || 3003;
+const server = express();
+const PORT = 3001;
+const mysql = require("mysql2");
+server.use(express.json());
 
-app.get('/', (req,res) => {
+server.get('/', (req,res) => {
     res.send('This is the Express Backend');
 });
 
-app.listen(PORT, () => {
+const db = mysql.createConnection({
+    user: "root",
+    password: "",
+    host: "localhost",
+    database: "ingredientsDB",
+  });
+
+server.get("placeholder", (req, res) => {
+    db.query("SELECT * FROM placeholder", (err, results) => {
+        if (err) {
+          console.log(err);
+        } else {
+          res.send(results);
+        }
+      });
+})
+
+server.post("/create", (req, res) => {
+    const first = req.body.name;
+    const second = req.body.name;
+    const third = req.body.name;
+    const fourth = req.body.name;
+
+    db.query(
+        "INSERT INTO placeholder (street_address, bedrooms, bathrooms, photo) VALUES (?,?,?,?)",
+        [first, second, third, fourth],
+        (err, result) => {
+          if (err) {
+            console.log(err);
+          } else {
+            res.send(result);
+          }
+        }
+      );
+})
+
+server.delete("/delete/:id", (req, res) => {
+    const id = req.params.id;
+  
+    db.query("DELETE FROM placeholder WHERE id = ?", id, (err, result) => {
+      if (err) {
+        console.log(err)
+      } else {
+        res.send(result);
+      }
+    })
+  })
+
+
+server.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
